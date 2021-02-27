d4f2817901a0d1e905c29b0b12b9441f1d624c29
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-02-27 19:14:57 +0530

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,4 +1,7 @@
 <?php
 session_start();
-$con=mysqli_connect('localhost','root','','leave_management_system');
+//development
+//$con=mysqli_connect('localhost','root','','leave_management_system');
+//live
+$con=mysqli_connect('remotemysql.com','yf2EdWQHaw','LuQSMJrwgo','yf2EdWQHaw');
 ?>
